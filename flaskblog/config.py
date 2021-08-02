9dff7d1fba8c0db05863b97e051763c7085ff098
Sir Eggnog <kendle.lam@gmail.com>
2021-08-02 02:24:34 -0400

    “commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,8 +1,10 @@
 import os
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = '681cb3b56395fcc937ab940f15dc66c5'
     CKEDITOR_PKG_TYPE = 'basic'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     
