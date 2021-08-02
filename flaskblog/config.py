1dbdaf548a9c24fed3a4c0679908670ac7a9240a
Sir Eggnog <kendle.lam@gmail.com>
2021-08-02 01:46:07 -0400

    “commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,7 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     CKEDITOR_PKG_TYPE = 'basic'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
-        'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     
