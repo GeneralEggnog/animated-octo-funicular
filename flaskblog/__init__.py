bf0203d8e97f5d562fdf7fc069c044dfe7012930
Sir Eggnog <kendle.lam@gmail.com>
2021-08-02 00:41:10 -0400

    “commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,45 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
+from flask_bcrypt import Bcrypt
+from flaskblog.config import Config
 
-app = Flask(__name__)
 
-from flaskblog import routes
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.admin'
+login_manager.login_message_category = 'info'
+http_auth = HTTPBasicAuth()
+
+
+
+users = {
+    "admin": generate_password_hash("blogpass123"),
+}
+
+
+
+
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
