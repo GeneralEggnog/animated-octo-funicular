b025f989f768d573066e5609e193f3ce1c2be6bd
Sir Eggnog <kendle.lam@gmail.com>
2021-08-25 12:18:21 -0400

    ds

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SECRET_KEY = '681cb3b56395fcc937ab940f15dc66c5'
     CKEDITOR_PKG_TYPE = 'basic'
-    SQLALCHEMY_DATABASE_URI = 'postgres://mrjbuifvfrfotv:4e08df0ec6fdbe522e94a3bdce34f9de12f5f6aec48cc78723cadb3a82ad82d5@ec2-3-237-55-96.compute-1.amazonaws.com:5432/djb3lh6cu3p2q'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mrjbuifvfrfotv:4e08df0ec6fdbe522e94a3bdce34f9de12f5f6aec48cc78723cadb3a82ad82d5@ec2-3-237-55-96.compute-1.amazonaws.com:5432/djb3lh6cu3p2q'
     
