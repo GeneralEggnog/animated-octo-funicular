3367e5597c1ed2a80a1afca5797be2d25669671f
Sir Eggnog <kendle.lam@gmail.com>
2021-08-25 13:38:29 -0400

    add new admin

--- add_admin.py
+++ add_admin.py
@@ -1,4 +1,4 @@
-from flaskblog import db, bcrypt
+from flaskblog import db, bcrypt, create_app
 from flaskblog.models import User
 
 command = input('1. Add\n2. Delete\n')
@@ -6,23 +6,24 @@ if command == '1':
     username = input('Username: ')
     password = input('Password: ')
 
-    user = User.query.filter_by(username=username).first()
-    if user:
-        print('User already exists.')
-    else:
-        user_to_add = User(username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
-
-        db.session.add(user_to_add)
-        db.session.commit()
+    with create_app().app_context():
+        user = User.query.filter_by(username=username).first()
+        if user:
+            print('User already exists.')
+        else:
+            user_to_add = User(username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
+            db.session.add(user_to_add)
+            db.session.commit()
 
     print("Successfully added new Administrator!")
 elif command=='2':
     username = input('Username: ')
     user = User.query.filter_by(username=username).first()
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        print("Successfully deleted Administrator!")
-    else:
-        print('No user by that name exists.')
+    with create_app().app_context():
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            print("Successfully deleted Administrator!")
+        else:
+            print('No user by that name exists.')
 
