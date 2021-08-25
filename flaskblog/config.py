f13ea7c50a8ddf1136bfa6686572bc3e75593c2b
Sir Eggnog <kendle.lam@gmail.com>
2021-08-25 11:45:47 -0400

    postgres

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SECRET_KEY = '681cb3b56395fcc937ab940f15dc66c5'
     CKEDITOR_PKG_TYPE = 'basic'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
