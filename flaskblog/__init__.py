4681ce439c13b9b8dd09282ad51dea8c70dd3a54
Sir Eggnog <kendle.lam@gmail.com>
2021-09-13 14:39:27 -0400

    lol

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -41,6 +41,5 @@ def create_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
-    app.app_context().push()
     
     return app
