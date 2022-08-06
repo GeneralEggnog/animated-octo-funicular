2562edbe4505e13b006dc5434a3e29a32e8bed0a
GeneralEggnog <kendle.lam@gmail.com>
2022-08-06 01:54:54 -0400

    add admin changes

--- add_admin.py
+++ add_admin.py
@@ -1,30 +1,34 @@
 from flaskblog import db, bcrypt, create_app
 from flaskblog.models import User
 
-command = input('1. Add\n2. Delete\n')
-if command == '1':
-    username = input('Username: ')
-    password = input('Password: ')
+while True:
+    command = input('1. Add\n2. Delete\n3. Cancel\n')
+    if command == '1':
+        username = input('Username: ')
+        password = input('Password: ')
 
-    with create_app().app_context():
-        user = User.query.filter_by(username=username).first()
-        if user:
-            print('User already exists.')
-        else:
-            user_to_add = User(username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
-            db.session.add(user_to_add)
-            db.session.commit()
-
-    print("Successfully added new Administrator!")
-elif command=='2':
-    username = input('Username: ')
-    
-    with create_app().app_context():
-        user = User.query.filter_by(username=username).first()
-        if user:
-            db.session.delete(user)
-            db.session.commit()
-            print("Successfully deleted Administrator!")
-        else:
-            print('No user by that name exists.')
-
+        with create_app().app_context():
+            user = User.query.filter_by(username=username).first()
+            if user:
+                print('User already exists.')
+            else:
+                user_to_add = User(username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
+                db.session.add(user_to_add)
+                db.session.commit()
+                print("Successfully added new Administrator!")
+        break
+        
+    elif command=='2':
+        username = input('Username: ')
+        
+        with create_app().app_context():
+            user = User.query.filter_by(username=username).first()
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+                print("Successfully deleted Administrator!")
+            else:
+                print('No user by that name exists.')
+        break
+    elif command=='3':
+        break
